0adcb0759bc2506a0da886b971dc19846bdace77
hemeilong <hemeilong@globalegrow.com>
2020-03-24 23:27:45 +0800

    add method testcase

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -1,4 +1,5 @@
 import unittest
+
 from python.calc import Calc
 
 
@@ -7,8 +8,17 @@ class TestCalc(unittest.TestCase):
     def setUp(self) -> None:
         self.calc = Calc()
 
-    def test_add(self):
+    def test_add_1(self):
         self.assertEqual(3, self.calc.add(1, 2))
 
     def test_add_2(self):
+        self.assertEqual(0, self.calc.add(0, 0))
+
+    def test_add_3(self):
         self.assertEqual(0.03, self.calc.add(0.01, 0.02))
+
+    def test_add_4(self):
+        self.assertEqual(-3, self.calc.add(-1, -2))
+
+    def test_add_5(self):
+        self.assertEqual("111222", self.calc.add("111", "222"))
