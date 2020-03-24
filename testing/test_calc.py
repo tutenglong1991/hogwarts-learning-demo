f28af4bdbdb4d577ce072ff3a72af949b7413963
hemeilong <hemeilong@globalegrow.com>
2020-03-25 00:05:36 +0800

    add method more testCase commit

--- testing/test_calc.py
+++ testing/test_calc.py
@@ -22,3 +22,23 @@ class TestCalc(unittest.TestCase):
 
     def test_add_5(self):
         self.assertEqual("111222", self.calc.add("111", "222"))
+
+    def test_add_6(self):
+        self.assertEqual([1, 2], self.calc.add([1], [2]))
+
+    def test_add_7(self):
+        self.assertEqual((1, 2, 3, 4), self.calc.add((1, 2), (3, 4)))
+
+    def test_add_8(self):
+        self.assertNotEqual((1, 2, 3, 4), self.calc.add((1, 3), (2, 4)))
+
+    # TypeError: unsupported operand type(s) for +: 'set' and 'set'
+    # def test_add_9(self):
+    #     self.assertNotEqual({1, 2, 3}, self.calc.add({1, 3}, {2, 3}))
+
+    # def test_add_10(self):
+    #     self.assertEqual({"a": 1, "b": 2}, self.calc.add({"a", 1}, {"b", 2}))
+
+
+if __name__ == '__main__':
+    unittest.main()
